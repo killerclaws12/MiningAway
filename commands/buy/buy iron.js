1bc65922f68614a8975c5a1941b3fc3074ec3bff
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2021-04-17 22:55:50 -0400

    Create buy iron.js

---
+++ commands/buy/buy iron.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const economy = require('../../economy.js')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  commands: ['buy iron tools', 'Buy iron tools'],
+  minArgs: 0,
+  maxArgs: null,
+  cooldown: 3,
+  callback: async(message,arguments,text) => {
+    const userId = message.author.id
+    const coinspresent = await economy.getCoins(userId)
+    var price = 500000
+
+    if(coinspresent > 500000) {
+      message.channel.send('Purchase Successful')
+      const remainingCoins = await economy.addCoins(
+      userId,
+      price * -1
+    )
+      var birchrandom = 1
+      await economy.addItems( userId, birchrandom, 'irontool')
+    } 
+  }
+}
