c7f5ddba8cca25b7b743f1902bd0056c7ed1eeaf
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2021-04-25 20:20:08 -0400

    Create botinfo.js

---
+++ commands/botinfo.js
@@ -0,0 +1,51 @@
+const economy = require('../economy.js')
+const Discord = require('discord.js')
+const { MessageEmbed } = require('discord.js')
+const os = require('os')
+module.exports = {
+    commands: ['botinfo', 'info'],
+    minArgs: 0,
+    maxArgs: 100,
+    cooldown: 3,
+    callback: async(message, arguments, text, client) => {
+      const userID = message.author.id
+        const embed = new MessageEmbed()
+            .setTitle('Bot Stats')
+            .setColor('#000000')
+            .addFields(
+                {
+                    name: '🌐 Servers',
+                    value: `Serving ${client.guilds.cache.size} servers.`,
+                    inline: true
+                },
+                {
+                    name: '📺 Channels',
+                    value: `Serving ${client.channels.cache.size} channels.`,
+                    inline: true
+                },
+                {
+                    name: '👥 Server Users',
+                    value: `Serving ${client.users.cache.size}`,
+                    inline: true
+                },
+                {
+                    name: '⏳ Ping',
+                    value: `${Math.round(client.ws.ping)}ms`,
+                    inline: true
+                },
+                {
+                    name: 'Join Date',
+                    value: client.user.createdAt,
+                    inline: true
+                },
+                {
+                    name: 'Server Info',
+                    value: `Cores: ${os.cpus().length}`,
+                    inline: true
+                }
+            )
+            .setFooter(`Created By: ${message.author.tag}`, message.author.displayAvatarURL())
+
+        await message.channel.send(embed)
+    }
+}
